425cd503d3d643efa15dbca7efc667d2edeb1ff2
Manuel Fernández <manferlo81@gmail.com>
2020-11-20 22:53:04 +0700

    chore(test): Test for error format

--- __test__/resolve-failed.test.ts
+++ __test__/resolve-failed.test.ts
@@ -3,7 +3,8 @@ import { resolveFailed } from '../src';
 describe('Resolve Failed', () => {
 
   test('Should always throw', () => {
-    expect(() => resolveFailed(null)).toThrow();
+    expect(() => resolveFailed(null)).toThrow(/^null/);
+    expect(() => resolveFailed('string')).toThrow(/^"string"/);
   });
 
 });
