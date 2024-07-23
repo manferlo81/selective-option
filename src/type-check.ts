6a026c381a6f1727540688ff717a6d2eb002d5ae
Manuel Fernández <manferlo81@gmail.com>
2024-07-23 15:42:56 +0700

    chore: Improve isArray type

--- src/type-check.ts
+++ src/type-check.ts
@@ -1,1 +1,12 @@
-export const { isArray } = Array as { isArray: <T = unknown>(value: unknown) => value is T[] };
+import type { TypeCheckFunction } from './helper-types';
+
+interface IsArrayFunction extends TypeCheckFunction<unknown[]> {
+  <T extends unknown[]>(value: unknown): value is T;
+  <T>(value: unknown): value is T[];
+}
+
+interface ExtendedArrayConstructor extends ArrayConstructor {
+  isArray: IsArrayFunction;
+}
+
+export const { isArray } = Array as ExtendedArrayConstructor;
