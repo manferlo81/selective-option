5057c5a7697b2486c549b1a51faec39440ba9bae
Manuel Fernández <manferlo81@gmail.com>
2024-07-23 13:21:35 +0700

    chore: Group deprecated functions for easier future removal

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,5 @@
 export { createResult } from './create-result';
-export { resolveArray } from './deprecated/resolve-array';
-export { resolveBoolBased } from './deprecated/resolve-bool-based';
-export { resolveNullish } from './deprecated/resolve-nullish';
-export { resolveObject } from './deprecated/resolve-object';
-export { resolveString } from './deprecated/resolve-string';
-export { resolveValue } from './deprecated/resolve-value';
-export { resolveValueBased } from './deprecated/resolve-value-based';
+export * from './deprecated/functions';
 export { createArrayResolver } from './resolve-array';
 export { createBoolBasedResolver } from './resolve-bool-based';
 export { resolveFailed } from './resolve-failed';
