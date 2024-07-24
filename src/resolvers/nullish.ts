27ad8ad1b78d87753d461195b40cfc217063e4bc
Manuel Fernández <manferlo81@gmail.com>
2024-07-24 19:21:13 +0700

    chore: Cleanup nullish resolver

--- src/resolvers/nullish.ts
+++ src/resolvers/nullish.ts
@@ -1,21 +1,21 @@
 import { createResult } from '../create-result';
 import type { PotentialResolver } from '../types';
 
-export function createNullishResolver<K extends string, D>(
+export function createNullishResolver<K extends string, V>(
   keys: readonly K[],
-  defaultValue: D,
-): PotentialResolver<K, D> {
+  value: V,
+): PotentialResolver<K, V> {
 
   // return resolver
-  return (value) => {
+  return (input) => {
 
     // exit if value is not nullish
-    if (value != null) return;
+    if (input != null) return;
 
     // return default result
     return createResult(
       keys,
-      defaultValue,
+      value,
     );
 
   };
