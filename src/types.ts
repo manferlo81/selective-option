e65069bf1cb4c13c9b4e71306b8dacab9e73d3df
Manuel Fernández <manferlo81@gmail.com>
2024-07-24 09:07:41 +0700

    feat(type): Allow input type on Resolve type

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,7 @@
-export type InputResolver<I, R> = (input: I) => R;
+/** @deprecated */
 export type ResolverBase<R> = InputResolver<unknown, R>;
+
+export type InputResolver<I, R> = (input: I) => R;
 export type Resolved<K extends string, V> = Readonly<Record<K, V>>;
-export type PotentialResolver<K extends string, V> = ResolverBase<Resolved<K, V> | void>;
-export type Resolver<K extends string, V> = ResolverBase<Resolved<K, V>>;
+export type PotentialResolver<K extends string, V> = InputResolver<unknown, Resolved<K, V> | void | undefined>;
+export type Resolver<K extends string, V, I = unknown> = InputResolver<I, Resolved<K, V>>;
