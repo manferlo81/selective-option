2e2060ad3eb8252f67e55d6b7c87e91c54019dd6
Manuel Fernández <manferlo81@gmail.com>
2024-07-26 23:25:22 +0700

    feat: Allow nullish as value validator in boolean based resolver to resolve boolean only

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -8,22 +8,22 @@ const rule = (options, type = 'error') => [type, options];
 const rules = {
   'no-useless-rename': 'error',
   'object-shorthand': 'error',
-  'no-multiple-empty-lines': rule({
+
+  '@stylistic/linebreak-style': rule('windows'),
+  '@stylistic/indent': rule(2),
+  '@stylistic/semi': rule('always'),
+  '@stylistic/quotes': rule('single'),
+  '@stylistic/comma-dangle': rule('always-multiline'),
+  '@stylistic/member-delimiter-style': rule({}),
+  '@stylistic/quote-props': rule('as-needed'),
+  '@stylistic/arrow-parens': rule('always'),
+  '@stylistic/padded-blocks': 'off',
+
+  '@stylistic/no-multiple-empty-lines': rule({
     max: 1,
     maxBOF: 0,
     maxEOF: 0,
   }),
-
-  '@stylistic/linebreak-style': 'off',
-
-  '@stylistic/comma-dangle': rule('always-multiline'),
-  '@stylistic/quotes': rule('single'),
-  '@stylistic/semi': rule('always'),
-  '@stylistic/indent': rule(2),
-  '@stylistic/member-delimiter-style': rule({}),
-  '@stylistic/padded-blocks': 'off',
-
-  '@stylistic/quote-props': rule('as-needed'),
 
   '@typescript-eslint/array-type': rule({
     default: 'array-simple',
--- src/resolvers/bool-based.ts
+++ src/resolvers/bool-based.ts
@@ -7,18 +7,73 @@ import { createStringResolver } from './string';
 import type { BoolBasedResolver, KeyList, SpecialKeys } from './types';
 import { createValueResolver } from './value';
 
+function wrapValueValidator<V>(isValidValue: AllowNullish<TypeCheckFunction<V>>): TypeCheckFunction<V | boolean> {
+  const isBoolean: TypeCheckFunction<boolean> = (value) => {
+    return value === true || value === false;
+  };
+  if (!isValidValue) return isBoolean;
+  const isValueOrBool: TypeCheckFunction<V | boolean> = (value) => {
+    return isValidValue(value) || isBoolean(value);
+  };
+  return isValueOrBool;
+}
+
 export function createBoolBasedResolver<K extends string, S extends string, V, O extends string>(
   keys: KeyList<K>,
   isValidValue: TypeCheckFunction<V>,
+  defaultValue: V | boolean,
+  overrideKey: O,
+  special: SpecialKeys<S, K>,
+): BoolBasedResolver<K, S, V | boolean, O>;
+
+export function createBoolBasedResolver<K extends string, V, O extends string>(
+  keys: KeyList<K>,
+  isValidValue: TypeCheckFunction<V>,
+  defaultValue: V | boolean,
+  overrideKey: O,
+  special: AllowNullish<never>,
+): BoolBasedResolver<K, never, V | boolean, O>;
+
+export function createBoolBasedResolver<K extends string, V, O extends string>(
+  keys: KeyList<K>,
+  isValidValue: TypeCheckFunction<V>,
+  defaultValue: V | boolean,
+  overrideKey: O,
+): BoolBasedResolver<K, never, V | boolean, O>;
+
+export function createBoolBasedResolver<K extends string, S extends string, O extends string>(
+  keys: KeyList<K>,
+  isValidValue: AllowNullish<never>,
+  defaultValue: boolean,
+  overrideKey: O,
+  special: SpecialKeys<S, K>,
+): BoolBasedResolver<K, S, boolean, O>;
+
+export function createBoolBasedResolver<K extends string, O extends string>(
+  keys: KeyList<K>,
+  isValidValue: AllowNullish<never>,
+  defaultValue: boolean,
+  overrideKey: O,
+  special: AllowNullish<never>,
+): BoolBasedResolver<K, never, boolean, O>;
+
+export function createBoolBasedResolver<K extends string, O extends string>(
+  keys: KeyList<K>,
+  isValidValue: AllowNullish<never>,
+  defaultValue: boolean,
+  overrideKey: O,
+): BoolBasedResolver<K, never, boolean, O>;
+
+export function createBoolBasedResolver<K extends string, S extends string, V, O extends string>(
+  keys: KeyList<K>,
+  isValidValue: AllowNullish<TypeCheckFunction<V>>,
   defaultValue: V | boolean,
   overrideKey: O,
   special?: AllowNullish<SpecialKeys<S, K>>,
 ): BoolBasedResolver<K, S, V | boolean, O> {
 
   // create boolean value validator
-  const isValueOrBool = (value: unknown): value is V | boolean => {
-    return isValidValue(value) || value === true || value === false;
-  };
+  const isValueOrBool = wrapValueValidator(isValidValue);
 
   // create potential resolvers
   const resolveValue = createValueResolver(keys, isValueOrBool);
