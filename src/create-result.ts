c38f1eab0a8d33c61a460c6c976ced1b649b9bed
Manuel Fernández <manferlo81@gmail.com>
2024-07-27 00:07:48 +0700

    chore(types): Improve function signatures

--- src/create-result.ts
+++ src/create-result.ts
@@ -2,6 +2,7 @@ import type { KeyList, Resolved } from './resolvers/types';
 
 export function createResult<K extends string, V>(keys: KeyList<K>, value: V): Resolved<K, V>;
 export function createResult<K extends string, V>(keys: KeyList<K>, value: V, input: Resolved<K, V>): Resolved<K, V>;
+export function createResult<K extends string, V>(keys: KeyList<K>, value: V, input?: Resolved<K, V>): Resolved<K, V>;
 export function createResult<K extends string, V>(keys: KeyList<K>, value: V, input?: Resolved<K, V>): Resolved<K, V> {
   return keys.reduce((output, key) => {
     return { ...output, [key]: value };
