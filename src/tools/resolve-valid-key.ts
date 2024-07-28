badf7b9dd5c9484e262ffb8274db46fdad9dc96c
Manuel Fernández <manferlo81@gmail.com>
2024-07-28 18:36:39 +0700

    chore: Remove unnecessary step on key resolving

--- src/tools/resolve-valid-key.ts
+++ src/tools/resolve-valid-key.ts
@@ -1,13 +1,39 @@
 import type { KeyList, AllowNullish, SpecialKeys } from '../private-types';
 import { is } from './is';
-import { resolveKey_v2 } from './key';
+import { resolveKey } from './key';
 
-export function resolveKeyIfValid<K extends string, S extends string>(key: unknown, keys: KeyList<K>, special?: AllowNullish<SpecialKeys<S, K>>): [keys: K[], value: boolean] | undefined {
+type KeyResolved<K extends string> = [keys: K[], value: boolean];
+
+export function resolveKeyIfValid<K extends string, S extends string>(key: unknown, keys: KeyList<K>, special?: AllowNullish<SpecialKeys<S, K>>): KeyResolved<K> | undefined {
 
   // throw if item is not a string
   if (!is(key, 'string')) return;
 
-  // try to resolve as key or special key
-  return resolveKey_v2(key, keys, special);
+  // trim key
+  const trimmedKey = key.trim();
+
+  // try to resolve positive key
+  const resolved = resolveKey(trimmedKey, keys, special);
+
+  // return positive key result if resolved
+  if (resolved) return [resolved, true];
+
+  // test for negative key format
+  const matched = /^[!-]([^\s]*)$/.exec(trimmedKey);
+
+  // fail if it doesn't match negative format
+  if (!matched) return;
+
+  // get key from matched result
+  const [matchedKey] = matched.slice(1);
+
+  // try to resolve matched key
+  const matchedResolved = resolveKey(matchedKey, keys, special);
+
+  // fail if it can't be resolved
+  if (!matchedResolved) return;
+
+  // return negative key result
+  return [matchedResolved, false];
 
 }
