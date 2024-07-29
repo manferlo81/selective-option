4dd51adc1528fb2764708ff7a5dea91ea68d66a6
Manuel Fernández <manferlo81@gmail.com>
2024-07-29 14:36:39 +0700

    chore: Fix internal Array.prototype.includes error

--- src/tools/key.ts
+++ src/tools/key.ts
@@ -1,9 +1,15 @@
 import type { AllowNullish, KeyList, SpecialKeys } from '../private-types';
 
+declare global {
+  interface ReadonlyArray<T> {
+    includes<E>(searchElement: E, fromIndex?: number): searchElement is E & T;
+  }
+}
+
 export function resolveKey<K extends string, S extends string>(key: string, keys: KeyList<K>, special: SpecialKeys<S, K>): K[] | undefined;
 export function resolveKey<K extends string>(key: string, keys: KeyList<K>, special?: AllowNullish<SpecialKeys<string, K>>): K[] | undefined;
-export function resolveKey<K extends string>(key: string, keys: KeyList<K>, special?: AllowNullish<SpecialKeys<string, K>>): K[] | undefined {
-  if (keys.includes(key as K)) return [key as K];
+export function resolveKey<K extends string>(key: string, keys: KeyList<K>, special?: AllowNullish<Partial<SpecialKeys<string, K>>>): K[] | undefined {
+  if (keys.includes(key)) return [key];
   if (!special) return;
-  return special[key] as K[] | undefined;
+  return special[key];
 }
