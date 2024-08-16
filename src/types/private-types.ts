862b75517c6ef4e9201674ba7109a30f2e58c100
Manuel Fernández <manferlo81@gmail.com>
2024-08-16 10:43:57 +0700

    chore(types): Nullish type doesn't contain void

--- src/types/private-types.ts
+++ src/types/private-types.ts
@@ -3,7 +3,7 @@ import type { KeyList } from './resolver-types';
 // eslint-disable-next-line @typescript-eslint/no-invalid-void-type
 export type Void = void;
 
-export type Nullish = null | undefined | Void;
+export type Nullish = null | undefined;
 export type AllowNullish<T> = T | Nullish;
 
 export type KeyResolved<K extends string> = [keys: KeyList<K>, isPositive: boolean];
